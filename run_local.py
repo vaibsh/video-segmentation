3555e38a6d37a6ef74fdcde6a24bb20f8e811259
Vaibhav Shinde <vaibsh@gmail.com>
2026-05-12 14:56:19 +0530

    run_local.py modified

--- run_local.py
+++ run_local.py
@@ -1,12 +1,43 @@
+import argparse
 from pipeline.runner import run_pipeline
 
 
+# ============================================================
+# MAIN
+# ============================================================
+
 def main():
 
-    run_pipeline(
-        video_file="inputs/gym.mp4",
-        all_frames_data_file="inputs/all_frames_data.pkl.gz"
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--video_file",
+        type=str,
+        required=True
     )
 
+    parser.add_argument(
+        "--pkl_gz_file",
+        type=str,
+        required=True
+    )
+
+    args = parser.parse_args()
+
+    # ========================================================
+    # SEGMENTATION
+    # ========================================================
+
+    run_pipeline(
+        video_file=args.video_file,
+        all_frames_data_file=args.pkl_gz_file
+    )
+
+
+# ============================================================
+# ENTRY
+# ============================================================
+
 if __name__ == "__main__":
+
     main()
